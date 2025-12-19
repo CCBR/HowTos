edbb0f198e6b064ad3a31a1acafd7f01de07f7ab
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-12-19 11:22:14 -0500

    better screenshots

--- docs/ai-ml/gh-pilot-vs-code/demo/setup.sh
+++ docs/ai-ml/gh-pilot-vs-code/demo/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mkdir data
+mkdir -p data
 scp helix:/data/CCBR_Pipeliner/db/PipeDB/Indices/GTFs/hg38/gencode.v48.primary_assembly.annotation.gtf ./data
 scp helix:/data/CCBR_Pipeliner/db/PipeDB/Indices/hg38_basic/Chromsomes/chrom.sizes ./data
